0b563c69cea7b3eac14ce9bfffa599ff28e4d445
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-04 15:50:40 -0700

    create useCharacters hook

---
+++ src/hooks/useCharacters.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import { fetchCharacters } from '../services/harryPotterApi';
+
+export const useCharacters = () => {
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetchCharacters()
+      .then((characters) => setCharacters(characters))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return [loading, characters];
+};
